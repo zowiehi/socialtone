c85eb83576543649bf42ef5ad39e8e710207a5e6
zowiehi <zdh27@nau.edu>
2017-02-25 15:39:05 -0700

    add twitter and tone analysis

--- app/apis/twitter.py
+++ app/apis/twitter.py
@@ -1,15 +1,15 @@
 
-import tweepy
-
-
-auth = tweepy.OAuthHandler('1BDeT59IM2QhW4C4w1LrkWvH3', 'v7xTClJOl5ckwnvzNzvs79BWZTlECeVK8EVjwFgFQlm8MAPX7N')
-auth.set_access_token('836883541-2yjoPLmxZyQQqbNXBELFvxB5Ia4rT7VVe1xFLEDH', 'eQxanWaPPn8RVFfRUKbBb9IQvOiHLMX2NzTgjpgA1dF4h')
-
-api = tweepy.API(auth)
+from chiedza.settings import TWEEPY_API as api
 
 
 def get_tweets(req):
-    results = api.search(q=req, rpp=10, page=1)
+    results = api.search(q=req, rpp=100, page=1)
     tweets = []
+    count = 0
     for status in results:
-        
+        if(count > 20):
+            break
+        if(status.favorite_count > 10):
+            tweets.append(status.text)
+
+    return tweets
