96c4e1811f57c9b6975d9549e9de09fb064bc9be
Jorden <jakereps@users.noreply.github.com>
2017-02-26 00:37:32 -0700

    Fix?

--- app/views.py
+++ app/views.py
@@ -19,27 +19,18 @@ class ResultsPage(TemplateView):
 def scrape_all(request):
     if request.GET:
         query = request.GET.get('search')
-        # results = analyze(query)
-        # Result.objects.create(
-        #     query=query,
-        #     positive=results.get('positive'),
-        #     anger=results.get('anger'),
-        #     sadness=results.get('sadness'),
-        #     disgust=results.get('disgust'),
-        #     percent_positive=results.get('percent_positive'),
-        #     percent_negative=results.get('percent_negative')
-        # )
-        results = {
-            'positive': 0.15,
-            'anger': 0.15,
-            'sadness': 0.15,
-            'disgust': 0.15,
-            'percent_positive': 0.6,
-            'percent_negative': 0.4,
-            'top_tweets': [
-                '<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">Just checked in at Starbucks (Starbucks Coffee 神田駅前店) — <a href="https://t.co/629tnpyIq6">https://t.co/629tnpyIq6</a></p>— 👤 Ben Guild (@benguild) <a href="https://twitter.com/benguild/status/835741731509325825">February 26, 2017</a></blockquote>',
-                '<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">Just checked in at Starbucks (Starbucks Coffee 神田駅前店) — <a href="https://t.co/629tnpyIq6">https://t.co/629tnpyIq6</a></p>— 👤 Ben Guild (@benguild) <a href="https://twitter.com/benguild/status/835741731509325825">February 26, 2017</a></blockquote>',
-                '<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">Just checked in at Starbucks (Starbucks Coffee 神田駅前店) — <a href="https://t.co/629tnpyIq6">https://t.co/629tnpyIq6</a></p>— 👤 Ben Guild (@benguild) <a href="https://twitter.com/benguild/status/835741731509325825">February 26, 2017</a></blockquote>'
-            ]
-        }
+        results = analyze(query)
+        # results = {
+        #     'positive': 0.15,
+        #     'anger': 0.15,
+        #     'sadness': 0.15,
+        #     'disgust': 0.15,
+        #     'percent_positive': 0.6,
+        #     'percent_negative': 0.4,
+        #     'top_tweets': [
+        #         '<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">Just checked in at Starbucks (Starbucks Coffee 神田駅前店) — <a href="https://t.co/629tnpyIq6">https://t.co/629tnpyIq6</a></p>— 👤 Ben Guild (@benguild) <a href="https://twitter.com/benguild/status/835741731509325825">February 26, 2017</a></blockquote>',
+        #         '<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">Just checked in at Starbucks (Starbucks Coffee 神田駅前店) — <a href="https://t.co/629tnpyIq6">https://t.co/629tnpyIq6</a></p>— 👤 Ben Guild (@benguild) <a href="https://twitter.com/benguild/status/835741731509325825">February 26, 2017</a></blockquote>',
+        #         '<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">Just checked in at Starbucks (Starbucks Coffee 神田駅前店) — <a href="https://t.co/629tnpyIq6">https://t.co/629tnpyIq6</a></p>— 👤 Ben Guild (@benguild) <a href="https://twitter.com/benguild/status/835741731509325825">February 26, 2017</a></blockquote>'
+        #     ]
+        # }
         return HttpResponse(json.dumps(results), content_type="application/json")
