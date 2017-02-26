eeb6c291d2cd926ef1f0d4c9731a9e3f6521539d
zowiehi <zdh27@nau.edu>
2017-02-26 00:06:27 -0700

    rebase

--- app/apis/_twitter.py
+++ app/apis/_twitter.py
@@ -3,7 +3,7 @@ from chiedza.settings import TWEEPY_API as api
 
 
 def get_tweets(req):
-    results = api.search(q=req, rpp=100)
+    results = api.search(q=req, lang='en' rpp=100)
     sorted(results, key=lambda x: x.favorite_count)
     tweets = []
     yield from results
