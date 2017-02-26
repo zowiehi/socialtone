e6696039664c05a402df8985529a1556e9004951
zowiehi <zdh27@nau.edu>
2017-02-25 17:21:20 -0700

    Add chart.js for visualizing data

--- app/apis/_twitter.py
+++ app/apis/_twitter.py
@@ -4,12 +4,13 @@ from chiedza.settings import TWEEPY_API as api
 
 def get_tweets(req):
     results = api.search(q=req, rpp=100, page=1)
+    results.sort(lambda x: x.favorite_count)
     tweets = []
     count = 0
     for status in results:
         if(count > 20):
             break
         if(status.favorite_count > 10):
-            tweets.append(status.text)
+            tweets.append(status)
 
     return tweets
